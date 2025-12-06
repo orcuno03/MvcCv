59f00680abf9edb00efb50d1ae78cc970a6f1a5b
Orçun Özşen <orcunozsen12@gmail.com>
2025-12-06 11:03:49 +0300

    Add project files.

---
+++ MvcCv/Repositories/DeneyimRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MvcCv.Models.Entity;
+
+namespace MvcCv.Repositories
+{
+    public class DeneyimRepository : GenericRepository<TblDeneyimlerim>
+    {
+        
+    }
+}
